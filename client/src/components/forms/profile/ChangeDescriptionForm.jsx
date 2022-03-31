d55719e92eb6a01e175df6ae303908168063417d
Alexandr <bysees33@gmail.com>
2022-04-01 02:12:39 +0300

    [application] - improved control for submit data

--- client/src/components/forms/profile/ChangeDescriptionForm.jsx
+++ client/src/components/forms/profile/ChangeDescriptionForm.jsx
@@ -1,5 +1,5 @@
 import { useForm } from 'react-hook-form'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import cn from 'classnames'
 
 import Input from '../fields/Input'
@@ -16,13 +16,28 @@ import profileStyles from './profileForms.module.scss'
 const ChangeDescriptionForm = () => {
 
   const dispatch = useDispatch()
-  const { formState: { errors }, handleSubmit, register } = useForm()
+  const { formState: { errors }, handleSubmit, register, reset } = useForm()
   const [updateInfo, { isLoading, error: serverError }] = useUpdateInfoMutation()
 
+  const { currentDescription, currentName } = useSelector(state => ({
+    currentDescription: state.user.description,
+    currentName: state.user.name,
+  }))
+
   const onSubmit = async (formData) => {
+    if (!formData.name && !formData.description) {
+      return
+    }
+    if (formData.name === currentName && formData.description === currentDescription) {
+      return
+    }
+
+    formData.name = formData.name || currentName
+    formData.description = formData.description || currentDescription
 
     const response = await updateInfo(formData)
     if (!response.error) {
+      reset()
       const { description, name } = response.data
       dispatch(setUserInfo({ description, name }))
     }
