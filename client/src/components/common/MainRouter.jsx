929a4595aaddc81a1745e048e1115f15b67ffebe
Alexandr <bysees33@gmail.com>
2022-04-11 15:01:08 +0300

    attempt to deploy

--- client/src/components/common/MainRouter.jsx
+++ client/src/components/common/MainRouter.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const MainRouter = ({ children }) => {
 
-  console.log("process.env.REACT_APP_ENV: ", process.env.REACT_APP_ENV)
-  if (process.env.REACT_APP_ENV?.trim() === 'production') {
+  console.log("process.env.NODE_ENV: ", process.env.NODE_ENV)
+  if (process.env.NODE_ENV?.trim() === 'production') {
     console.log("HashRouter")
     return (
       <HashRouter basename={process.env.PUBLIC_URL}>
