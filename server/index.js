cd0d5178a888d5e0cd23a5c5610ad77cb4eee998
Alexandr <bysees33@gmail.com>
2022-04-11 07:50:16 +0300

    attempt to deploy

--- server/index.js
+++ server/index.js
@@ -21,17 +21,17 @@ function setExpressStaticFiles() {
   })
 }
 
-// setExpressStaticFiles()
+setExpressStaticFiles()
+app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use('/api', router)
+
 if (process.env.NODE_ENV?.trim() === 'production') {
   app.use(express.static(resolve(__dirname, '..', 'client', 'build')))
   app.get('/*', (req, res) => {
     res.sendFile(resolve(__dirname, '..', 'client', 'build', 'index.html'))
   })
 }
-
-app.use(cors())
-app.use(json())
-app.use(urlencoded({ extended: true }))
-app.use('/api', router)
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
