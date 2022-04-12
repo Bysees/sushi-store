6c853364e864bf9889374ddac98915dda31e875d
Alexandr <bysees33@gmail.com>
2022-04-12 05:29:31 +0300

    [application]
    Added a handler for deleting products from the cart when deleting a category

--- client/src/components/forms/filters/DeleteCategoryButton.jsx
+++ client/src/components/forms/filters/DeleteCategoryButton.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
-import { useDeleteCategoryMutation } from '../../../redux/RTKquery/category'
 import { appRoutes } from '../../../consts/links'
+import { useDeleteCategoryMutation } from '../../../redux/RTKquery/category'
+import { useGetProductsQuery } from '../../../redux/RTKquery/product'
+import { removeFromCart } from '../../../redux/slices/cart'
+
 
 const DeleteCategoryButton = ({ className, category }) => {
 
+  const dispatch = useDispatch()
+  const cartItems = useSelector(state => state.cart.cartItems)
   const navigate = useNavigate()
+  const { data: products } = useGetProductsQuery(category)
   const [deleteCategory, { isLoading }] = useDeleteCategoryMutation()
+
+  const deleteOrderedProductsFromCart = () => {
+    const categoryProducts = products?.find(({ category: itemsCategory }) => itemsCategory.eng === category)
+    const productItems = categoryProducts?.items
+
+    cartItems.forEach(cartItem => {
+      const productItem = productItems.find(productItem => productItem.id === cartItem.id)
+
+      if (!!productItem) {
+        dispatch(removeFromCart({ id: cartItem.id, amount: cartItem.amount }))
+      }
+    })
+  }
 
   const removeCategory = async () => {
     const isRemove = window.confirm('Все товары в категории будут безвозвратно потеряны, всё ровно удалить?')
@@ -17,6 +37,7 @@ const DeleteCategoryButton = ({ className, category }) => {
 
       if (!response.error) {
         navigate(appRoutes.menu)
+        deleteOrderedProductsFromCart()
       } else {
         alert(response.error.data.message)
       }
